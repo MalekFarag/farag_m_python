4a36278d215e748434bb576203fa146297af242b
Farag <m_farag@tss-hr420-dm13.local>
2018-10-16 14:41:55 -0400

    created .py file and coded small portion of RPS game

---
+++ RPSGame.py
@@ -0,0 +1,11 @@
+# type hash to make gay notes
+
+from random import randint
+
+choices = ["Rock", "Paper", "Scissors"]
+	#to make chocies
+
+computer_choice = choices[randint(0,2)]
+						#"randint to choose at random
+
+print("computer chooses:", computer_choice) 
